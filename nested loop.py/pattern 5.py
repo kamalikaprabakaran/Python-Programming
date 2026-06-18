b6b9b971eecdd1cfc425c2b57da573a4c2a4b978
YourGitHubUsername <your-email@example.com>
2026-06-18 21:58:57 +0530

    Add pattern 4.py

---
+++ nested loop.py/pattern 5.py
@@ -0,0 +1,10 @@
+def staircase(n):
+    # Write your code here
+    for row in range(1,n+1):
+        for space in range(n-row):
+            print(" ",end='')
+        for hash in range(row):
+            print("#",end='')
+        print()
+n =int(input("Enter a number:"))
+print(staircase(n))
